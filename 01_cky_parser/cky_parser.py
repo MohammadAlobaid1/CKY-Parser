621b3e7c5c3fdd3ef20b74c118817a3e9521a70b
Mohammad Alobaid <m@Mohammads-MacBook-Pro.local>
2020-05-07 00:16:27 -0400

    I did both the parser and the recognizer in the same code

--- 01_cky_parser/cky_parser.py
+++ 01_cky_parser/cky_parser.py
@@ -1,40 +1,146 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-def cky_parser(string, grammar):
-    """CKY parser that returns a chart with backpointers.
-
+def cky_recognize(string, grammar, lexicon):
+    """CKY recognizer to determine well-formedness of string.
     The string is assumed to be tokenized and lowercased already, e.g.
     ["the", "man", "water", "s", "the", "flower", "s"]
     """
-    pass
+    matrix = cky_matrix(string, grammar, lexicon)
+    #if you reach final and is S return True 
+    if "S" in (x[1] for x in matrix[0][len(string)-1]) :
+      return True 
+    else:
+      return False
 
+
+def cky_matrix(string, grammar, lexicon):
+    """A CKY recognizer that return a matrix and a backpointer.
+    The string is assumed to be tokenized and lowercased already, e.g.
+    ["the", "man", "water", "s", "the", "flower", "s"]
+    """
+    backpointer = {}
+    cky_list = []
+    string_len = len(string)
+    matrix = [[[] for x in range(string_len)] for y in range(string_len)]
+    
+
+    for index in range(string_len):
+      if not lexicon.get(string[index],None):
+        return []
+      #save the leafs
+      for x in lexicon[string[index]]:
+        matrix[index][index].append((index,x,index+1))
+        #save the leaf nodes as None
+        if backpointer.get((index,index)) == None:
+          backpointer[(index,index)] = [(x,None,None)]
+        else:
+          backpointer[(index,index)].append((x,None,None))
+
+        #if backpointer.get((index,index)) == None else backpointer[(index,index)].append((x,None,None))
+
+    i = 1
+    j = 0
+    start_x = 0 
+    start_y = 1
+    done = False   
+    while not done: 
+      string_len += -1
+      x = start_x
+      y = start_y
+
+      for index in range(string_len):
+        #print("string_len: ", string_len)
+        keep_iter = True
+
+        while keep_iter:
+          #print("i: ", i , " j: ", j, " x: ", x, " y: ", y)
+          #matrix[x][x+j] transition1 (row)
+          #matrix[x+i][y] transition 2 (col)
+          for transition1 in matrix[x][x+j]:
+            for transition2 in matrix[x+i][y]:
+              
+              #print("state1: ", state1, "state2: ",state2)
+              state1 = grammar.get(transition1[1],None)
+        
+              if state1:
+                state2 = grammar.get(transition1[1],None).get(transition2[1],None)
+                if state2:
+                  matrix[x][y].append((transition1[0],state2,transition2[2]))
+                
+                  index1 = [t[0]for t in backpointer[(x,x+j)]].index(transition1[1] )
+                  index2 = [t[0]for t in backpointer[(x+i,y)]].index(transition2[1] )
+                  
+                  if backpointer.get((x,y)) == None:
+                    backpointer[(x,y)]= [(state2,((x,x+j),index1),((x+i,y),index2))]
+                  else:
+                    backpointer[(x,y)].append((state2,((x,x+j),index1),((x+i,y),index2)))                      
+          i+=1
+          j+=1
+
+          if x+j == y:
+            keep_iter = False
+
+        x += 1
+        y += 1
+        i = 1
+        j = 0
+
+      start_y += 1
+
+      if string_len < 1:
+        done = True
+      
+    log = ""
+    y = 0
+
+    for x in range(len(string)):
+       log +=  "|\t" + str(x) + "\t"
+
+    for i in range(len(matrix)):
+      log += "\n"
+      log += "--------------------------------------------------------------------------------------\n"
+      log +=  str(i) + "|"
+      y  += 1 
+
+      for i2 in range(len(matrix[i])):
+        log += ", ".join([str(x[1])  for x in matrix[i][i2]]) + "\t\t|" 
+    
+    print(log)
+    print(backpointer)
+    return matrix
 
 def convert_grammar(grammar):
-    # if you want to modify the grammar,
-    # change this function
-    return grammar
+    '''A method to convert grammar to dict'''
+    new_grammar = {} 
+    for (q1, (symbol1, symbol2)) in grammar:
+      if symbol1 in new_grammar:
+        #if there is symbol add it along with the previous symbol
+        new_grammar[symbol1].update({symbol2: q1})
+      else:
+        new_grammar[symbol1] = {symbol2: q1}
+        
+
+    return new_grammar
 
 
 def convert_lexicon(lexicon):
-    # if you want to modify the lexicon,
-    # change this function
-    return lexicon
-
-
-def extract_parsetrees(chart):
-    """Convert the CKY chart into a list of parse trees."""
-    # this is optional!
-    pass
+    '''A method to convert lexicon to dict'''
+    new_lexicon = {}
+    for key,value in lexicon.items():
+      for word in value:
+        if word in new_lexicon:
+          #if there is symbol add it along with the previous symbol
+          new_lexicon[word].append(key)
+        else:
+          new_lexicon[word] = [key]
+           
+    return new_lexicon
 
 
 def test_cky(sentence=None):
     """Test cky_recognize.
-
     You can either specify your own sentence as a string,
     or use the default test sentences.
     """
+
     lexicon = {
         "A": ["old", "former", "alleged", "handsome", "big", "ugly"],
         "Adv": ["very", "quickly", "allegedly", "today"],
@@ -54,6 +160,7 @@ def test_cky(sentence=None):
         ("AP", ("Adv", "AP")),
         ("AP", ("AdvP", "AP")),
         ("D'", ("Poss", "NP")),
+        ("D'", ("Poss", "N")),
         ("DP", ("Det", "N")),
         ("DP", ("Det", "NP")),
         ("DP", ("DP", "D'")),
@@ -91,12 +198,13 @@ def test_cky(sentence=None):
         "every opera singer 's water on some very very old woman 's balcony very quickly man s the boat today": True,
         "": False,
         "John sleep s": False,
-        ]
+        }
     if sentence:
-        return cky_recognize(sentence)
+      	  
+        return cky_recognize(sentence, grammar, lexicon)
     else:
         for s, val in sentences.items():
-            if cky_recognize(s.split()) != val:
+            if cky_recognize(s.split(), grammar, lexicon) != val:
                 print("Wrong output!")
                 print(f"The following sentence should be {val}")
                 print(s)
